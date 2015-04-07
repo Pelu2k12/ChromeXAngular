566f0f870bee562cd22a4f20417b80b83fecb4a1
Arian Caraballo <arian.caraballo@bookit.com>
2015-04-07 18:02:05 -0400

    clean up

--- js/app/popup.js
+++ js/app/popup.js
@@ -2,14 +2,8 @@ myApp.controller("PageController", ['$scope', '$compile', '$element', function($
     $scope.counter = 0;
     this.addTimer = function (){
         $scope.counter++;
-        // // console.log(document.getElementById("foo");
-        // var newTimerNode = $compile( "<timer></timer>" )( $scope );
-        // $element(document.getElementById('timerDiv')).append(newTimerNode);
         angular.element(document.getElementById('timerDiv')).append(
             $compile("<timer id='"+$scope.counter+"'></timer>")($scope)
         );
-        // $element(document.getElementById('timerDiv').appendChild(newTimerNode);
-
-        // document.getElementById('timerDiv').appendChild(document.createElement("timer"));
     };
 }]);
