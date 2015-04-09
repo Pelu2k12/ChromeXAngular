7709ef2087f167986ab6212763fe28eb706b29d9
Arian Caraballo <arian.caraballo@bookit.com>
2015-04-09 14:14:20 -0400

    clean up

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,11 @@
 (function (ng){
-    var myApp = ng.module('TaskTimer', ['timer']).controller("PageController", function($scope) {
+    var myApp = ng.module('TaskTimer', ['timer']).controller("PageController", ['$scope', '$compile', '$element', function($scope, $compile, $element) {
         $scope.counter = 0;
-    });
+        this.addTimer = function (){
+            $scope.counter++;
+            ng.element(document.getElementById('timerDiv')).append(
+                $compile("<timer id='"+$scope.counter+"'></timer>")($scope)
+            );
+        };
+    }]);
 })(angular);
