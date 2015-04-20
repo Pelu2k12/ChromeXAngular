6620ff55bf5834875e3c4ecc44624972bae51ce8
Arian Caraballo <ariacaraballo@gmail.com>
2015-04-20 00:53:32 -0400

    working on the create form

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,15 @@
 (function (ng){
     var myApp = ng.module('TaskTimer', ['timer']).controller("PageController", ['$scope', '$compile', '$element', function($scope, $compile, $element) {
+        
+        var self = this;
+        
         $scope.counter = 0;
         this.addTimer = function (){
             $scope.counter++;
+            
+            console.log(self.project + "--" + self.task);
             ng.element(document.getElementById('timerDiv')).append(
-                $compile("<timer id='"+$scope.counter+"'></timer>")($scope)
+                $compile("<timer id='"+$scope.counter+"' ></timer>")($scope)
             );
         };
     }]);
